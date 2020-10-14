5980f8caa62ba911754c588f6ffa336ec9a79f64
刘健 <17602656463@163.com>
2020-10-14 16:56:13 +0800

    :fire: antd table

---
+++ src/router/antd/table-dnd.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import DndTable from "../../components/DndTable";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+  },
+];
+
+const list = [
+  {
+    key: "1",
+    name: "John Brown 1",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+  },
+  {
+    key: "2",
+    name: "Jim Green 2",
+    age: 42,
+    address: "London No. 1 Lake Park",
+  },
+  {
+    key: "3",
+    name: "Joe Black 3",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+  },
+  {
+    key: "4",
+    name: "Joe Black 4",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+  },
+  {
+    key: "5",
+    name: "Joe Black 5",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+  },
+];
+
+export default class DragSortingTable extends Component {
+  state = {
+    list,
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        list,
+      });
+    }, 2000);
+  }
+
+  handleDragEnd = (newList) => {
+    this.setState({
+      list: newList,
+    });
+  };
+
+  render() {
+    return (
+      <DndTable
+        tableProps={{ columns }}
+        data={this.state.list}
+        onDragEnd={this.handleDragEnd}
+      />
+    );
+  }
+}
